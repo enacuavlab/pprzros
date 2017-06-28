a272282ce053ca94a56a1c46d035e7bf6df097f6
Unknown <charlelie.michaud@free.fr>
2017-06-28 10:20:56 +0200

    First small parser and Slamdunk messages added

--- pprzros_msgs/ros2pprzros.py
+++ pprzros_msgs/ros2pprzros.py
@@ -2,7 +2,26 @@ from lxml import etree
 import os
 
 message_file = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                          "../msg/messages.xml"))
+                                                          "msg/messages.xml"))
 
-def creator(message_name):
+
+def parser(message_name):
     tree = etree.parse(message_file)
+    root = tree.getroot()
+    spacing = ""
+    def rec_parser(root, message_name, spacing):
+        for the_class in root:
+            for the_message in the_class:
+                att = the_message.attrib["name"]
+                if message_name == att:
+                    for the_attribute in the_message:
+                        att_att = the_attribute.attrib
+                        print spacing + att_att["name"]
+                        if att_att["type"][:2] == "m_":
+                            rec_parser(root, att_att["type"][2:], spacing + "-")
+        return 0
+    rec_parser(root, message_name, spacing)
+
+if __name__ == '__main__':
+    parser("POSE_STAMPED")
+
